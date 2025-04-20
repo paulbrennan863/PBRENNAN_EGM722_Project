affddb278584c9353472c3b28fe494572db07f37
paulbrennan863 <paulbrennan863@gmail.com>
2025-04-20 22:54:28 +0100

    Added in Dataframe display on GUI and CSV file save option and general tidyup

--- PBRENNAN_EGM722_V2.py
+++ PBRENNAN_EGM722_V2.py
@@ -1,11 +1,3 @@
-#import time
-#import boto3
-#from datetime import datetime, timezone
-#import json
-#from os import listdir
-#from os.path import isfile, join
-#import urllib3
-#import requests
 import os
 import xml.etree.ElementTree as ET
 import csv
@@ -14,30 +6,32 @@ import geopandas as gpd
 from shapely.geometry import Point, Polygon
 import folium
 from folium.plugins import MiniMap
+import webbrowser
 
 import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog as fd
-#from tkinter.messagebox import showinfo
+
 
 gui = tk.Tk()
-ElementTag = []
-ElementText = []
-EquipmentIDArray = []
-EquipmentLat = []
-EquipmentLong = []
+#ElementTag = []
+#ElementText = []
+#EquipmentIDArray = []
+#EquipmentLat = []
+#EquipmentLong = []
 
-xmlfilename = "orbcomm6.xml"
+xmlfilename = "orbcomm6.xml" #preload with xml test filename
 machine_data = []  # global list for parsed xml data
+joined_machine_pos_bedrock = ""
 
 
 def create_folium_map():
-    global m
-    m = folium.Map(location=[54.6, -7], zoom_start=9)
+    global m # lat function lnow it is the global map instance being called
+    m = folium.Map(location=[54.6, -7], zoom_start=9) # centre the map around Northern Ireland
     mini_map = MiniMap()
     m.add_child(mini_map)
 
-def select_file():
+def select_file_to_open():
     global xmlfilename # define the the xmlfile name is the global one to be modified
     filetypes = (('XML files','*.xml'),)
     filename = fd.askopenfilename(title='Open XML file', initialdir='/',filetypes=filetypes)
@@ -49,45 +43,87 @@ def select_file():
     load_bedrock_shapefile()
 
 
-    #showinfo(title='Selected File', message=filename)
 
 
-def printFilename():
-    print(xmlfilename)
+def save_dataframe_as_csv(dfcsv):
+    filetypes = (('CSV files', '*.csv'),)
+    filename = fd.asksaveasfilename(title='Save DataFrame as CSV', initialdir='/', defaultextension=".csv",filetypes=filetypes)
+
+    if filename:
+        # Drop geometry column for writing to CSV file as we have columns for Lat & Long already
+        dfcsv = joined_machine_pos_bedrock.drop(columns='geometry', errors='ignore')
+        dfcsv.to_csv(filename, index=False)
 
 
-#create the root window
+
+#write the machine dataframe to a CSV file
+#def WriteDataframetoCSV():
+#    print(joined_machine_pos_bedrock)
+#    print(joined_machine_pos_bedrock)
+
+
+#write the machine dataframe to the GUI frame
+def WriteDataframetoGUI(machinedataframe, joined_machine_pos_bedrock):
+    # Drop geometry column for display as we have columns for Lat & Long already
+    dfgui = joined_machine_pos_bedrock.drop(columns='geometry', errors='ignore')
+
+    # adjust the style values to making headings bold and left justified
+    style = ttk.Style()
+    style.configure("Treeview.Heading", font=('Arial', 10, 'bold'), anchor='w')  # 'w' = (west) left-justify
+
+    tree = ttk.Treeview(machinedataframe, columns=list(dfgui.columns), show='headings')
+
+    # Add column headers
+    for col in dfgui.columns:
+        tree.heading(col, text=col, anchor='w')
+        tree.column(col, width=150)
+
+    for _, row in dfgui.iterrows():
+        tree.insert("", "end", values=list(row))
+
+    #tree.pack(fill='both', expand=True)
+    tree.place(x=10, y=50, height=220, width=1450)
+
+
+
+
+#create the main GUI window
 def CreateGUIframe():
 
-    gui.title('Tkinter Open File Dialog')
+    gui.title('AEMP 2.0 Machine Viewer')
     gui.resizable(False, False)
-    gui.geometry('300x150')
+    gui.geometry('1500x300')
     gui.attributes('-topmost', True) #make sure the GUI fram is on top to ensure it is seen
+
+    machinedataframe=tk.Frame(gui,borderwidth=2, relief=tk.RIDGE)
+    machinedataframe.place(x=10, y=50, height=220, width=1450)
+
+
+
+
 
 def CreateGUIfileopenbutton():
     #open button
     open_button = ttk.Button(
     gui,
     text='Open a AEMP2.0 xml File',
-    command=select_file
+    command=select_file_to_open # call the select file to open dialouge function
     )
-    open_button.pack(expand=True)
+    open_button.place(x=10, y=10)
 
-def CreateGUIfileprintoutput():
-    #open button
-    print_button = ttk.Button(
+def CreateGUICSVSavebutton():
+    #CSV Save button
+    global WriteDataframtoCSV
+    save_csv_button = ttk.Button(
     gui,
-    text='Print Name of xml File',
-    command=printFilename
+    text='Save data to CSV file',
+    command=lambda: save_dataframe_as_csv(joined_machine_pos_bedrock)
     )
-    print_button.pack(expand=True)
+    save_csv_button.place(x=175, y=10)
 
 
 
 def xml_pasrse(filename):
-    #xmlscriptFile = open(filename)
-    #print(xmlscriptFile)
-    #xmlparse = ET.parse(xmlscriptFile)
     global machine_data
 
     xmlparse = ET.parse(filename)
@@ -131,14 +167,15 @@ def xml_pasrse(filename):
             fuelconsumed ='0'  # If no value is found in fuel branch the addin void
 
         # Calculate the amount of Carbon Dioxide produced by each machine in tonnes
-        Co2_produced = ((float(fuelconsumed) * 2.68)/1000) #Each litre of Diesel produces 2.68kg  of C02 during combusion then divide by 1000 to get co2 tonnes
+        CO2_produced = (float(fuelconsumed) * 2.680)/1000 #Each litre of Diesel produces 2.68kg  of CO2 during combusion then divide by 1000 to get CO2 tonnes
 
         # Append data to the list
-        machine_data.append({'OEMName': OEMname, 'Model': model, 'SerialNumber': serialNumber, 'Latitude': latitude, 'Longitude': longitude,'Hour': hour,'FuelConsumed': fuelconsumed, 'Fuel Unit': fuelunits, 'C02 Produced Kg': ("%.2f"%Co2_produced)})
+        machine_data.append({'OEM Name': OEMname, 'Model': model, 'Serial Number': serialNumber, 'Latitude': latitude, 'Longitude': longitude,'Operating Hours': hour,'Fuel Consumed (litres)': fuelconsumed, 'Fuel Unit': fuelunits, 'C02 output (tonne)': ("%.2f"%CO2_produced)})
 
 
 
 def load_bedrock_shapefile():
+    global joined_machine_pos_bedrock
     print(type(machine_data))  # should be list or dict
     df = gpd.GeoDataFrame(machine_data)
 
@@ -162,43 +199,31 @@ def load_bedrock_shapefile():
     bedrock.rename(columns={"RCS_D": "Bedrock Type"}, inplace=True)
 
     geometry = gpd.points_from_xy(df['Longitude'], df['Latitude'], crs='epsg:4326')
-    machine_positions = gpd.GeoDataFrame(df[['SerialNumber', 'Model', 'Hour', 'FuelConsumed', 'C02 Produced (tonnes))']], # use the csv data, but only the name/website columns
+    machine_positions = gpd.GeoDataFrame(df[['OEM Name','Serial Number', 'Model', 'Operating Hours', 'Fuel Consumed (litres)', 'C02 output (tonne)', 'Latitude', 'Longitude']], # add in the main columns from geo data frame of AEMP xml file
                             geometry=gpd.points_from_xy(df['Longitude'], df['Latitude']), # set the geometry using points_from_xy
                             crs='epsg:4326') # set the CRS using a text representation of the EPSG code for WGS84 lat/lon
     # Perform the spatial join, using 'contains' to find bedrock polygons that contain the machine position point
     joined_machine_pos_bedrock = gpd.sjoin(machine_positions, bedrock, predicate='within')
     joined_machine_pos_bedrock.drop(columns=['index_right', 'Shape_Leng', 'Shape_Area'], axis=1, inplace=True)
 
+    WriteDataframetoGUI(machinedataframe, joined_machine_pos_bedrock)
 
-    m = joined_machine_pos_bedrock.explore('SerialNumber', marker_type='marker', popup=True, legend=False, color='red')
+    m = joined_machine_pos_bedrock.explore('Serial Number', marker_type='marker', popup=True, legend=False, color='red')
     m = bedrock.explore('Bedrock Type', m=m, cmap='viridis', opacity=0.01, legend=False,)
     m.save('map2.html')
-    m.show_in_browser()
+    #m.show_in_browser()#
+    webbrowser.open_new_tab('map2.html')  # This is non-blocking way to show map without locking out tkinter GUI window
+
 
 
 
 #### Main progream starts here ###
 create_folium_map()
-CreateGUIframe()
+machinedataframe = CreateGUIframe()
 CreateGUIfileopenbutton()
-CreateGUIfileprintoutput()
-
-
-
-# run the application
+CreateGUICSVSavebutton()
+# run the application gui loop to keep tkinter running
 gui.mainloop()
-#import re
-#from datetime import date
-#from requests.auth import HTTPBasicAuth
-
-
-#def xml_branch_value_extract(img, pmin=0., pmax=100.):
-
-
-
-
-x#mlfilename = fd.askopenfilename()
-
 
 
 
